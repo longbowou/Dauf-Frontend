33289907821aecccbbe8eb22fa994201961fe0fc
longbowou <blandedaniel@gmail.com>
2022-09-12 07:03:18 +0000

    [update] theme mode store

--- src/layouts/main-layout/theme-mode/ThemeModeSwitcher.vue
+++ src/layouts/main-layout/theme-mode/ThemeModeSwitcher.vue
@@ -1,21 +1,21 @@
 <template>
   <!--begin::Menu-->
   <div
-    class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-title-gray-700 menu-icon-muted menu-active-bg menu-state-primary fw-semibold py-4 fs-base w-175px"
-    data-kt-menu="true"
-    data-kt-element="theme-mode-menu"
+      class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-title-gray-700 menu-icon-muted menu-active-bg menu-state-primary fw-semibold py-4 fs-base w-175px"
+      data-kt-menu="true"
+      data-kt-element="theme-mode-menu"
   >
     <!--begin::Menu item-->
     <div class="menu-item px-3 my-0">
       <router-link
-        :to="path"
-        :class="{ active: themeMode === 'light' }"
-        class="menu-link px-3 py-2"
-        @click="setMode('light')"
+          :to="path"
+          :class="{ active: themeMode === 'light' }"
+          class="menu-link px-3 py-2"
+          @click="setMode('light')"
       >
         <span class="menu-icon" data-kt-element="icon">
           <span class="svg-icon svg-icon-3">
-            <inline-svg src="media/icons/duotune/general/gen060.svg" />
+            <inline-svg src="media/icons/duotune/general/gen060.svg"/>
           </span>
         </span>
         <span class="menu-title">Light</span>
@@ -25,14 +25,14 @@
     <!--begin::Menu item-->
     <div class="menu-item px-3 my-0">
       <router-link
-        :to="path"
-        :class="{ active: themeMode === 'dark' }"
-        class="menu-link px-3 py-2"
-        @click="setMode('dark')"
+          :to="path"
+          :class="{ active: themeMode === 'dark' }"
+          class="menu-link px-3 py-2"
+          @click="setMode('dark')"
       >
         <span class="menu-icon" data-kt-element="icon">
           <span class="svg-icon svg-icon-3">
-            <inline-svg src="media/icons/duotune/general/gen061.svg" />
+            <inline-svg src="media/icons/duotune/general/gen061.svg"/>
           </span>
         </span>
         <span class="menu-title">Dark</span>
@@ -42,14 +42,14 @@
     <!--begin::Menu item-->
     <div class="menu-item px-3 my-0">
       <router-link
-        :to="path"
-        :class="{ active: themeMode === 'system' }"
-        class="menu-link px-3 py-2"
-        @click="setMode('system')"
+          :to="path"
+          :class="{ active: themeMode === 'system' }"
+          class="menu-link px-3 py-2"
+          @click="setMode('system')"
       >
         <span class="menu-icon" data-kt-element="icon">
           <span class="svg-icon svg-icon-3">
-            <inline-svg src="media/icons/duotune/general/gen062.svg" />
+            <inline-svg src="media/icons/duotune/general/gen062.svg"/>
           </span>
         </span>
         <span class="menu-title">System</span>
@@ -61,26 +61,25 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
-import { useStore } from "vuex";
-import { Actions } from "@/store/enums/StoreEnums";
-import { useRoute } from "vue-router";
+import {computed, defineComponent} from "vue";
+import {useRoute} from "vue-router";
+import {useThemeModeStore} from "@/stores/useThemeModeStore";
 
 export default defineComponent({
   name: "kt-theme-switcher",
   component: {},
   setup() {
-    const store = useStore();
+    const store = useThemeModeStore();
     const route = useRoute();
 
     const themeMode = computed(() => {
-      return store.getters.getThemeMode;
+      return store.getThemeMode;
     });
 
     const path = computed(() => route.path);
 
     const setMode = (mode: string) => {
-      store.dispatch(Actions.SET_THEME_MODE_ACTION, mode);
+      store.setThemeModeAction(mode);
     };
 
     return {
---
+++ src/stores/useThemeModeStore.ts
@@ -0,0 +1,28 @@
+import {defineStore} from "pinia";
+import {ThemeModeComponent} from "@/assets/ts/layout";
+
+
+const themeModeLSKey = "kt_theme_mode_value";
+const themeMenuModeLSKey = "kt_theme_mode_menu";
+
+export const useThemeModeStore = defineStore('theme-mode', {
+    state: () => ({
+        mode: localStorage.getItem(themeModeLSKey)
+            ? (localStorage.getItem(themeModeLSKey) as "light" | "dark" | "system")
+            : ThemeModeComponent.getSystemMode()
+    }),
+    getters: {
+        getThemeMode(): string {
+            return this.mode;
+        }
+    },
+    actions: {
+        setThemeModeAction(payload) {
+            localStorage.setItem(themeModeLSKey, payload);
+            localStorage.setItem(themeMenuModeLSKey, payload);
+            document.documentElement.setAttribute("data-theme", payload);
+            ThemeModeComponent.init();
+            this.mode = payload;
+        }
+    }
+})
