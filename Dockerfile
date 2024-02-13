c27b43132aa1e136a0a618fb4cbe2d1f0c075acb
Longbowou <blandedaniel@gmail.com>
2024-02-13 01:35:04 -0600

    Refactor & Update Readme !

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /app
 
 RUN yarn global add @vue/cli
 
+RUN groupmod -g 1000 node
+RUN usermod -u 1000 -g node node
+
 RUN chown node:node -R /app
 
 USER node
--- src/layouts/main-layout/footer/Footer.vue
+++ src/layouts/main-layout/footer/Footer.vue
@@ -3,8 +3,8 @@
   <div class="footer py-4 d-flex flex-lg-column" id="kt_footer">
     <!--begin::Container-->
     <div
-      class="d-flex flex-column flex-md-row align-items-center justify-content-between"
-      :class="{
+        class="d-flex flex-column flex-md-row align-items-center justify-content-between"
+        :class="{
         'container-fluid': footerWidthFluid,
         'container-xxl': !footerWidthFluid,
       }"
@@ -13,10 +13,10 @@
       <div class="text-dark order-2 order-md-1">
         <span class="text-muted fw-semobold me-1">2022 &copy;</span>
         <a
-          href="https://gitlab.com/longbowou"
-          target="_blank"
-          class="text-gray-800 text-hover-primary"
-        >Longbowou</a
+            href="https://gitlab.com/longbowou"
+            target="_blank"
+            class="text-gray-800 text-hover-primary"
+        >Dauf</a
         >
       </div>
       <!--end::Copyright-->
@@ -27,8 +27,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { footerWidthFluid } from "@/core/helpers/config";
+import {defineComponent} from "vue";
+import {footerWidthFluid} from "@/core/helpers/config";
 
 export default defineComponent({
   name: "KTFooter",
